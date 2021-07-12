2dbf7383eb274da2c11fd05572d009a13ff8b9a0
Jonathan <jonulisanpar@gmail.com>
2021-07-12 17:51:43 -0500

    Modificacion de carpetas y practica de navigation c
    Se realizo la practica de navigation Controller, esta presenta varias pantallas y 2 StoryBoards…

---
+++ Secion 5_Navegacion en las Apps/Navegacion/Navegacion/SegundoSB/PssViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PssViewController.swift
+//  Navegacion
+//
+//  Created by rruiz on 12/07/21.
+//
+
+import UIKit
+
+class PssViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func regresar(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Secion 5_Navegacion en las Apps/Navegacion/Navegacion/SegundoSB/SssViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SssViewController.swift
+//  Navegacion
+//
+//  Created by rruiz on 12/07/21.
+//
+
+import UIKit
+
+class SssViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
